840e945ffc2c119ae9813178f1bc0ee4c35cb4d2
markHRBL <mgorbal.pro@gmail.com>
2022-04-03 15:48:40 +0300

    added validation

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const numberOfFilms = +prompt('How many movies have you watched?', 0);
 const personalMovieDB = {
     count: numberOfFilms,
@@ -7,5 +9,26 @@ const personalMovieDB = {
     privat: false
 };
 
-personalMovieDB.movies[prompt('One of the last wached movies', '')] = prompt('Rate him', '1.0');
-personalMovieDB.movies[prompt('One of the last wached movies', '')] = prompt('Rate him', '1.0');
+let i = 0;
+
+while (i < 2) {
+    const movieQuestion = prompt('One of the last wached movies', '');
+    const rate = prompt('Rate him', '1.0');
+
+    if (rate && movieQuestion && movieQuestion.length < 50) {
+        personalMovieDB.movies[movieQuestion] = rate;
+        i++;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    alert('You watched very few movies');
+} else if (personalMovieDB.count < 30) {
+    alert('You are classic viewer');
+} else if (personalMovieDB.count >= 30) {
+    alert('You are moviegoer');
+} else {
+    alert('Eror');
+}
+
+console.log(personalMovieDB);
