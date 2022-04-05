67cc6c1d84d88ea6a26791690646c1e53f0f98e1
markHRBL <mgorbal.pro@gmail.com>
2022-04-05 14:24:51 +0300

    updated with functions

--- script.js
+++ script.js
@@ -1,6 +1,56 @@
 "use strict";
 
-const numberOfFilms = +prompt('How many movies have you watched?', 0);
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('How many movies have you watched?', 0);
+
+    while (numberOfFilms === '' || numberOfFilms === null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('How many movies have you watched?', 0);
+    }
+}
+
+function rememberMyFilms() {
+    let i = 0;
+
+    while (i < 2) {
+        const movieQuestion = prompt('One of the last wached movies', '');
+        const rate = prompt('Rate him', '1.0');
+
+        if (rate && movieQuestion && movieQuestion.length < 50) {
+            personalMovieDB.movies[movieQuestion] = rate;
+            i++;
+        }
+    }
+}
+
+function detectPersonalLelvel() {
+    if (personalMovieDB.count < 10) {
+        alert('You watched very few movies');
+    } else if (personalMovieDB.count < 30) {
+        alert('You are classic viewer');
+    } else if (personalMovieDB.count >= 30) {
+        alert('You are moviegoer');
+    } else {
+        alert('Eror');
+    }
+}
+
+function showMyDB() {
+    if (!personalMovieDB.privat) {
+        console.log(personalMovieDB);
+    }
+}
+
+function writeYourGenres() {
+    for (let i = 1; i < 4; i++) {
+        const question = prompt(`Your favorite genre by number ${i}`);
+        personalMovieDB.genres.push(question);
+    }
+}
+
+start();
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -9,26 +59,7 @@ const personalMovieDB = {
     privat: false
 };
 
-let i = 0;
-
-while (i < 2) {
-    const movieQuestion = prompt('One of the last wached movies', '');
-    const rate = prompt('Rate him', '1.0');
-
-    if (rate && movieQuestion && movieQuestion.length < 50) {
-        personalMovieDB.movies[movieQuestion] = rate;
-        i++;
-    }
-}
-
-if (personalMovieDB.count < 10) {
-    alert('You watched very few movies');
-} else if (personalMovieDB.count < 30) {
-    alert('You are classic viewer');
-} else if (personalMovieDB.count >= 30) {
-    alert('You are moviegoer');
-} else {
-    alert('Eror');
-}
-
-console.log(personalMovieDB);
+rememberMyFilms();
+detectPersonalLelvel();
+writeYourGenres();
+showMyDB();
